017f1015cecf32e428e68f971bf42e0c942325d3
maguiceri <magui.cerisola@gmail.com>
2020-07-14 09:30:22 -0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor (props){
+    super (props);
+    this.state={ // <- este objeto es la fuente de informacion 
+      inputValue:'',
+      tasks: []
+    }
+  }
+  
+  handleChange (e) { // <- uno de los actores que actualiza la fuente de informacion
+    this.setState({ // <- la funcion que vamos a utilizar para actualizar el estado 
+    inputValue: e.target.value, // <- actualizamos la propiedad inputValue con el valor de e.target.value
+    
+    })
+  }
+
+  handleClick () {
+    console.log (this.state.inputValue) //<- leemos la propiedad inputValue de nuestro estado
+
+    //leer la propiedad inputValue de nuestro estado
+    const newValue = this.state.inputValue
+
+    //leer la propiedad tasks de nuestro estado
+    const newTasks = this.state.tasks
+
+    //Agregarle a ese array un nuevo item con el valor de inputValue
+    newTasks.push(newValue)
+
+    //actualizar el estado taks con el nuevo array
+    this.setState ({
+      tasks : newTasks,
+      inputValue:'' // para q se borre el contenido del input cuando agregamos una tarea
+    })
+  }
+
+  
+
+  render() {
+
+    return(
+      <div>
+        <input value={this.state.inputValue} onChange = { (e) => this.handleChange (e) } type="text" />
+        <button onClick={ () => this.handleClick () }>agregar</button>
+      
+
+    <ul>
+    
+      {this.state.tasks.map ( (task, key) => {
+        return (
+          <li key={key}> {task} </li>
+        )
+
+      })}
+
+    </ul>
     </div>
-  );
+
+    )
+
+  }
+
 }
+
 
 export default App;
